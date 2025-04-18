2d70c830af3fc6b6d9d0dff4c3b7adb403318629
Sergiu Finciuc <sergio.finciuc@gmail.com>
2025-04-18 12:01:54 +0300

    Comment updated.

--- capri_tools/__init__.py
+++ capri_tools/__init__.py
@@ -1,9 +1,8 @@
-# Import all tools
 from capri_tools.read_file import read_file_tool
 from capri_tools.edit_file import edit_file_tool
 
 def get_all_tools():
-    """Return a list of all available tools"""
+    """Return a list of all available tools. Add new items for Claude to use."""
     return [
         read_file_tool,
         edit_file_tool
